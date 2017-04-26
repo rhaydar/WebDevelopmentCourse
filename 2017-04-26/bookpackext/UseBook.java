015f1480fcb388c1a768d459098ac668993cd52c
Rashedul Haydar <rhaydar8@gmail.com>
2017-04-26 14:43:29 -0400

    Did example on pg 273

---
+++ 2017-04-26/bookpackext/UseBook.java
@@ -0,0 +1,18 @@
+package bookpackext;
+
+class UseBook {
+	public static void main(String[] args) {
+		bookpack.Book[] books = new bookpack.Book[5];
+		int i;
+
+		books[0] = new bookpack.Book("Java: A Beginner's Guide", "Schildt", 2014);
+		books[1] = new bookpack.Book("Java: The Complete Reference", "Schildt", 2014);
+		books[2] = new bookpack.Book("The Art of Java", "Schildt and Holmes", 2003);
+		books[3] = new bookpack.Book("Red Storm Rising", "Clancy", 1986);
+		books[4] = new bookpack.Book("On The Road", "Kerouac", 1955);
+
+		for (i=0; i<books.length; i++) {
+			books[i].show();
+		}
+	}
+}
