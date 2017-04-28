88a4fc7029e177cd28d5fa667245ac51356df7c0
Rashedul Haydar <rhaydar8@gmail.com>
2017-04-28 11:28:53 -0400

    ExcDemo5 done

---
+++ 2017-04-28/ExcDemo5.java
@@ -0,0 +1,23 @@
+class ExcDemo5 {
+	public static void main(String[] args) {
+		int numerators[] = {7,42,69,99,5,12,21,10};
+		int denominators[] = {7,2,0,4,0,0};
+
+		for (int i=0; i<numerators.length; i++) {	
+			try {
+				System.out.println(numerators[i]/denominators[i]);
+			}
+			catch(ArithmeticException e) {
+				System.out.println("Division by zero!");
+			}
+			catch(ArrayIndexOutOfBoundsException e) {
+				System.out.println("Index out-of-bounds!");
+			}
+			catch(Throwable e) {
+				System.out.println("Some error occurred.");
+			}
+		}
+
+		System.out.println("After for-loop.");
+	}
+}
