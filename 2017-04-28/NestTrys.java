04b1d49c74882d698e3e44558263339c8972ecec
Rashedul Haydar <rhaydar8@gmail.com>
2017-04-28 11:41:47 -0400

    Adds NestTrys

---
+++ 2017-04-28/NestTrys.java
@@ -0,0 +1,22 @@
+class NestTrys {
+	public static void main(String[] args) {
+		int numerators[] = {7,42,69,99,5,12,21,10};
+		int denominators[] = {7,2,0,4,0,0};
+
+		try {
+			for (int i=0; i<numerators.length; i++) {	
+				try {
+					System.out.println(numerators[i]/denominators[i]);
+				}
+				catch(ArithmeticException e) {
+					System.out.println("Division by zero!");
+				}
+			}
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Index out-of-bounds!");
+		}
+		
+		System.out.println("After outer try-catch.");
+	}
+}
